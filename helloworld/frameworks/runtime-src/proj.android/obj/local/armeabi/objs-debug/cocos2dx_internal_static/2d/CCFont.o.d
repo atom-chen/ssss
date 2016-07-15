19f06ec505b35a1a296b986739b9548146470f52
reid <litredbear@gmail.com>
2016-07-15 17:06:43 +0800

    add

--- helloworld/frameworks/runtime-src/proj.android/obj/local/armeabi/objs-debug/cocos2dx_internal_static/2d/CCFont.o.d
+++
@@ -1,2 +0,0 @@
-obj/local/armeabi/objs-debug/cocos2dx_internal_static/2d/CCFont.o: \
- /Users/reid/Documents/templates/helloworld/frameworks/runtime-src/proj.android/../../cocos2d-x/cocos//./2d/CCFont.cpp
--- helloworld/frameworks/runtime-src/proj.android/obj/local/armeabi/objs-debug/cocostudio_static/CCArmatureDefine.o.d
+++
@@ -1,5 +0,0 @@
-obj/local/armeabi/objs-debug/cocostudio_static/CCArmatureDefine.o: \
- /Users/reid/Documents/templates/helloworld/frameworks/runtime-src/proj.android/../../cocos2d-x/cocos//editor-support/cocostudio/CCArmatureDefine.cpp \
- /Users/reid/Documents/templates/helloworld/frameworks/runtime-src/proj.android/../../cocos2d-x/cocos//editor-support/cocostudio/../cocostudio/CCArmatureDefine.h
-
-/Users/reid/Documents/templates/helloworld/frameworks/runtime-src/proj.android/../../cocos2d-x/cocos//editor-support/cocostudio/../cocostudio/CCArmatureDefine.h:
